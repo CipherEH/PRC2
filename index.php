9ccef55e93e82affde1840cf297bf7ea9829529e
quijanor41 <quijanor41@gmail.com>
2023-02-06 20:25:58 -0800

    samp commit

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
 <?php 
 
-    echo("Test One");
-    echo("Test Two");
-    echo("Test Three");
-    echo("Test Four");
-    echo("Test Five");
-    echo("Test Six");
-    echo("Test Seven");
+    echo("This is a test!!!");
 
 ?>
